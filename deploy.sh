48e592319937a0663819f9da832e24dba743ab88
LevHecny <l.savin@yolka.tech>
2023-03-12 09:32:42 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+git pull
 npm run build
 cp build/index.html build/404.html
 git add build -f
 git commit -m "adding build"
-git subtree push --prefix build origin gh-pages
+git subtree push --prefix build origin gh-pages-sh
