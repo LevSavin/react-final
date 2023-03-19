ff6e6d394440d3b1fbd0727cf29e2f9002e2482c
LevHecny <l.savin@yolka.tech>
2023-03-19 18:07:20 +0300

    refactor: добавил красоты в роутах

--- src/config/AppRoutes.tsx
+++ src/config/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { lazy } from "react";
 import { Routes, Route } from "react-router-dom"
 
@@ -12,11 +13,27 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path={`${process.env.REACT_APP_REPO}/`} element={<Layout/>}>
-        <Route index path={`${process.env.REACT_APP_REPO}/`} element={<HomePage/>}></Route>
-        <Route path={`${process.env.REACT_APP_REPO}/registration`} element={<RegisterPage/>}></Route>
-        <Route path={`${process.env.REACT_APP_REPO}/profile`} element={<ProfilePage/>}></Route>
-        <Route path={`${process.env.REACT_APP_REPO}/restaurant/:id`} element={<RestaurantPage/>}></Route>
-        <Route path="*" element={<NotFoundPage/>}></Route>
+        <Route
+          index
+          path={`${process.env.REACT_APP_REPO}/`}
+          element={<HomePage/>}
+        ></Route>
+        <Route
+          path={`${process.env.REACT_APP_REPO}/registration`}
+          element={<RegisterPage/>
+        }></Route>
+        <Route 
+          path={`${process.env.REACT_APP_REPO}/profile`}
+          element={<ProfilePage/>}
+        ></Route>
+        <Route
+          path={`${process.env.REACT_APP_REPO}/restaurant/:id`}
+          element={<RestaurantPage/>}
+        ></Route>
+        <Route
+          path="*"
+          element={<NotFoundPage/>}
+        ></Route>
       </Route>
     </Routes>
   );
